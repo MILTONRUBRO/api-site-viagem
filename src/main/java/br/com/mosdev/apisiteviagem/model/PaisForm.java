e321c57648aaccd7817a0455e3443d9c9c50a16e
Milton Oliveira Souza <miltonrubro@gmail.com>
2020-04-01 19:34:01 -0300

    Criação de um endpoint para cadastrar novo país

---
+++ src/main/java/br/com/mosdev/apisiteviagem/model/PaisForm.java
@@ -0,0 +1,21 @@
+package br.com.mosdev.apisiteviagem.model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class PaisForm {
+	
+	@NotNull
+	@NotEmpty
+	private String nome;
+
+	public Pais novoPais() {
+		return new Pais(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+
+}
