b6e5b6cb9011119747970206b22257c43e2ea947
Milton Oliveira Souza <miltonrubro@gmail.com>
2020-04-01 01:20:58 -0300

    Criação do setup do projeto com springboot

---
+++ src/main/java/br/com/mosdev/apisiteviagem/ApiSiteViagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.mosdev.apisiteviagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSiteViagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSiteViagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mosdev/apisiteviagem/ApiSiteViagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mosdev.apisiteviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSiteViagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
